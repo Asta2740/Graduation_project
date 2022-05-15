bee812309089f5692f44df4372664cc2417f6efb
Asta2740 <youssefhani2740@gmail.com>
2022-05-15 19:52:58 +0200

    changing it to z

--- shein2egypt/models/school.py
+++ shein2egypt/models/school.py
@@ -4,6 +4,6 @@ from odoo import fields,models
 class ScrapperProfile(models.Model):
     _name = "scrapper.profile"
 
-    name = fields.Char(string="scrapper name") #you will see scrapper name in gui as label name , if you dont give any name it will be name
+    name = fields.Char(string="scrapper namez") #you will see scrapper name in gui as label name , if you dont give any name it will be name
     email =fields.Char(string="email")
     phone =fields.Char(String="phone")

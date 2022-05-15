d7c32a0bb1a5c3daa1d163b05f46a5998a7bb863
Asta2740 <youssefhani2740@gmail.com>
2022-05-15 19:44:23 +0200

    our first commit

---
+++ shein2egypt/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name':'WebScrapper',
+    'version':'1.0',
+    'author':'youssef',
+    'summary':'Links to products',
+    'sequence':'1',
+    'description':"we will begin our journy now",
+    'category':'School',
+    'website':'sheesh',
+    'depends':['base'],
+    'data':[
+        "views/school_view.xml"
+    ]
+
+}

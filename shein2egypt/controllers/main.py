c50673b8252271a8352f87f7494a7cb3e4bce45f
Asta2740 <youssefhani2740@gmail.com>
2022-05-16 15:21:42 +0200

    soooo we learned how to inhert now we learn how to change

--- shein2egypt/controllers/main.py
+++ shein2egypt/controllers/main.py
@@ -1,10 +1,25 @@
 from odoo import http
 from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleInherit(WebsiteSale):
+    @http.route([
+        '''/shop''',
+        '''/shop/page/<int:page>''',
+        '''/shop/category/<model("product.public.category"):category>''',
+        '''/shop/category/<model("product.public.category"):category>/page/<int:page>'''
+    ], type='http', auth="public", website=True,)
+    def shop(self, page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post):
+        res = super(WebsiteSaleInherit, self).shop(page=0, category=None, search='', min_price=0.0, max_price=0.0,
+                                                   ppg=False, **post)
+        print("wtf am i doing")
+        return res
 
 
 class shein2egypt(http.Controller):
 
-    @http.route('/Shein2egypt', website=True, auth='public')
+    @http.route('/Shein2egypt', website=True, auth='user')
     def web_scrapper(self, **kw):
         return request.render("shein2egypt.Shein_page", {})
-        #return "hello world"
+        # return "hello world"

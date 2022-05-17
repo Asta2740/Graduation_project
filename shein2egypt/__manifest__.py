69a369c5bc90d010fc64d427f84c86c0bcde8106
Asta2740 <youssefhani2740@gmail.com>
2022-05-17 14:44:54 +0200

    we added the link to the main menu on the haeder

--- shein2egypt/__manifest__.py
+++ shein2egypt/__manifest__.py
@@ -1,14 +1,14 @@
 {
-    'name':'WebScrapper',
-    'version':'1.0',
-    'author':'youssef',
-    'summary':'Links to products',
-    'sequence':'1',
-    'description':"we will begin our journy now",
-    'category':'Sales/Sales',
-    'website':'sheesh',
-    'depends':['product'],
-    'data':[
+    'name': 'WebScrapper',
+    'version': '1.0',
+    'author': 'youssef',
+    'summary': 'Links to products',
+    'sequence': '1',
+    'description': "we will begin our journy now",
+    'category': 'Sales/Sales',
+    'website': 'sheesh',
+    'depends': ['product', 'website'],
+    'data': [
         'security/ir.model.access.csv',
         'views/template.xml',
         'views/inquiry.xml',
@@ -17,4 +17,4 @@
 
     ],
 }
-#a short idea of what is the module about
+# a short idea of what is the module about
